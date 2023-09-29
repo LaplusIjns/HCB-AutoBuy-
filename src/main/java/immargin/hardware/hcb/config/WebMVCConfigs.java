49effefecdaff2ec88443abd5c4762e355cbc645
Laplus <jack19960310@gmail.com.tw>
2023-09-29 23:00:43 +0800

    fix thymeleaf layout

--- src/main/java/immargin/hardware/hcb/config/WebMVCConfigs.java
+++ src/main/java/immargin/hardware/hcb/config/WebMVCConfigs.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+
 @Configuration
 public class WebMVCConfigs implements WebMvcConfigurer {
     
@@ -22,5 +24,10 @@ public class WebMVCConfigs implements WebMvcConfigurer {
         registrationBean.setOrder(1);
         return registrationBean;    
     }
+    
+    @Bean LayoutDialect layoutDialect() {
+        return new LayoutDialect();
+    }
+    
 
 }
